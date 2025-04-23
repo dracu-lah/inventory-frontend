e4552d5c8aff08745b069c243d33eb7aeb417396
dracu-lah <nevilnicks4321@gmail.com>
2025-04-23 14:31:54 +0530

    feat:added roboto font

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,10 @@ import { StrictMode } from "react";
 import Routes from "@/router/routes";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
 import ReactDOM from "react-dom/client";
 const queryClient = new QueryClient();
